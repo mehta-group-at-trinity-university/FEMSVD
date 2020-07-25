497ff1edb83a2e53b83522256bc122fc4def9f06
quantumwave <nmehta@trinity.edu>
2020-07-24 23:09:24 -0500

    Convergence seems to be slower than expected with respect to the number of elements

--- 4BFEMSVD.f90
+++ 4BFEMSVD.f90
@@ -1,33 +1,43 @@
+module datastructures
+  type irix
+     integer ir, ix
+     logical bridge
+  end type irix
+end module datastructures
 program SVD
+  use datastructures
   implicit none
-  real*8 n,w,m,Rmin,Rmax,res,temp,overlap,tempA,tempB,epsout
+  real*8 n,w,m,Rmin,Rmax,res,temp,overlap,tempA,tempB,epsout,res1,res2
   real*8 Shift,alpha,r0diatom,etaOVERpi,xMin,xMax,yMin,yMax,RDerivDelt,RFirst,RLast,V2Depth
   real*8 e,eMin,eMax,m1,m2,m3,m4,mu4,massarray(4)
-  real*8, allocatable :: nodesR(:),weightsR(:),nds(:),wts(:)
-  real*8, allocatable :: Bsparse(:),eigenVals(:),ress(:)
+  real*8, allocatable :: nodesR(:),weightsR(:),nds(:),wts(:),wmn(:,:),xmn(:,:)
+  real*8, allocatable :: eigenVals(:),RSectors(:)
   real*8, allocatable :: EVals(:),EVecs(:,:),beta(:)
-  real*8, allocatable :: dXr(:,:),TmatR(:,:),weightsDMR(:,:),S(:,:),eigenVecs(:,:)
+  real*8, allocatable :: dXrAll(:,:),dXr(:,:),fp(:,:,:),TmatTemp(:,:),TmatR(:,:),weightsDMR(:,:),S(:,:),eigenVecs(:,:)
   real*8, allocatable :: iPsi(:,:),jPsi(:,:),H(:,:),B(:,:)
-  real*8, allocatable :: Uad(:,:,:),Psi(:,:,:),O(:,:)
-  integer LobattoPoints
-  integer i,j,k,ivec,ifilenodes,ix,sR,sRc,row,mu,nu,inu,jmu,rCountA,numN0A,rCountB,numN0B,probSize
+  real*8, allocatable :: Uad(:,:,:),Psi(:,:,:),O(:,:), sectorT(:,:,:),wtemp(:,:,:), AllT(:,:,:,:)
+  integer LP,NRSectors
+  integer i,j,k,l,ivec,ifilenodes,ix,sR,sRc,row,mu,nu,inu,jmu,numN0A,numN0B,probSize,count
   integer xDim,yDim,ncv,matrixdim,HalfBandWidth,set
   integer NumStates,PsiFlag,CouplingFlag,LegPoints,order,Left,Right,Bottom,Top,xNumPoints,yNumPoints
-  integer fparam(64),info,loop,Tstart,Tend,rate
-  integer, allocatable :: Hrow(:),Hcol(:),Brow(:),Bcol(:),indexOf(:,:)
+  integer fparam(64),info,loop,Tstart,Tend,rate,iRS,jRS,in,im,jn,NRGridPoints
+  integer, allocatable :: Hrow(:),Hcol(:),Brow(:),Bcol(:),indexOf(:,:), lindex(:,:)
+  logical bridgebra, bridgeket
+  type(irix), allocatable :: rxindex(:)
   character*64 LegendreFile
   character*64 Outputfile
   CHARACTER*64 LobattoFile
   character*3 wfnfile
   character*3 wfnsuffix
   character*64 wfnout
-  integer iwfn
+  integer iwfn, l1, l2
   double precision, parameter :: Pi = 3.1415926535897932384626433832795
+  integer, external :: kdelta
   read(5,*)
   read(5,*) xNumPoints, yNumPoints
   read(5,*)
   read(5,*)
-  read(5,*) LobattoPoints, NumStates, Order
+  read(5,*) NRSectors, LP, NumStates, Order
   read(5,*)
   read(5,*)
   read(5,*) V2Depth, Rmin, Rmax, alpha
@@ -52,46 +62,147 @@ program SVD
   mu4=(m1*m2*m3*m4)**(0.25d0)
   write(6,*) "mu4 = ", mu4
   massarray = (/m1,m2,m3,m4/)
-  Outputfile = "Eigenvals.dat"!"eigenVals-100x100x100x20x5-HHHHOdd.dat"
+  Outputfile = "Eigenvals.dat"
   open(unit=1,file=Outputfile)
 
   call system_clock(Tstart)
 
-  sR=LobattoPoints
+  NRGridPoints=NRSectors+1
+  sR=(NRGridPoints-1)*LP-NRGridPoints+2
   sRc=sR-2
+  write(6,*) "sR = ", sR
+  write(6,*) "sRc = ", sRc
+  allocate(rxindex(sR))
+  allocate(nds(LP),wts(LP))
+  allocate(nodesR(sR),weightsR(sR),dXrAll(sR,sR),weightsDMR(sR,sR))
+  allocate(wmn(LP,NRSectors),xmn(LP,NRSectors),lindex(LP,NRSectors))
+  allocate(RSectors(NRSectors+1))
+  allocate(SectorT(LP,LP,NRSectors))
+  allocate(fp(LP,LP,NRSectors))
+  allocate(wtemp(LP,LP,NRSectors))
 
-  allocate(nds(sR),wts(sR),nodesR(sR),weightsR(sR),dXr(sRc,sR),weightsDMR(sR,sR),TmatR(sRc,sRc))
-
-  ! read in the Gauss-Lobatto nodes and weights.  Be sure LobattoPoints is equal to one of
+  call makeRSectors(RSectors,NRSectors, Rmin, Rmax)
+  ! read in the Gauss-Lobatto nodes and weights.  Be sure LP is equal to one of
   ! the options in the input following file:
   LobattoFile = 'GaussLobatto.dat'
-  call GetGaussLobattoFactors(LobattoFile,LobattoPoints,nds,wts)
+  call GetGaussLobattoFactors(LobattoFile,LP,nds,wts)
+  
+!  call rescale(sR,Rmin,Rmax,nds,wts,nodesR,weightsR)
+  i = 0
 
-  call rescale(sR,Rmin,Rmax,nds,wts,nodesR,weightsR)
- ! write(6,*) "scaled nodes:"
- ! call printmatrix(nodesR,1,sr,6)
- ! write(6,*) "scaled weights:"
- ! call printmatrix(weightsR,1,sr,6)
-  do i=1,sRc
-     do j=1,sR
-        call calcDX(nodesR,i+1,j,sR,weightsR,res)
-        dXr(i,j)=res
-     end do
-  end do
-  stop
-  
+  write(6,*) "setting all nodes:"
   weightsDMR=0d0
-  do i=1,sR
-     weightsDMR(i,i)=weightsR(i)!*nodesR(i)**2
-  end do
-  TmatR=0d0
-  TmatR = (0.5d0/mu4)*MATMUL(dXr,MATMUL(weightsDMR,TRANSPOSE(dXr)))
-!  write(6,*) "Kinetic energy matrix:"
-!  call printmatrix(TmatR,sRc,sRc,6)
+  wtemp=0d0
+! count = 0
+  do iRS = 1, NRSectors
+     write(6,*) "iRS = ", iRS, RSectors(iRS) 
+     call rescale(LP,RSectors(iRS),RSectors(iRS+1),nds,wts,xmn(:,iRS),wmn(:,iRS))
+     do jn = 1, LP
+        wtemp(jn,jn,iRS) = wmn(jn,iRS)
+        if((iRS.lt.NRSectors).and.(jn.eq.LP)) cycle
+!        count = count+1
+        i = (iRS-1)*LP-iRS+1+jn
+        lindex(jn,iRS) = i
+        rxindex(i)%ir = iRS
+        rxindex(i)%ix = jn
+        rxindex(i)%bridge = .false.
+        nodesR(i) = xmn(jn,iRS)
+        weightsR(i) = wmn(jn,iRS)
+ 
+        if((iRS.gt.1).and.(jn.eq.1)) then
+!           count = count - 1
+           weightsR(i) = wmn(LP,iRS-1) + wmn(1,iRS)
+           rxindex(i)%bridge = .true.
+        endif
+        weightsDMR(i,i) = weightsR(i)
+        !
+!        write(6,*) iRS, jn, xmn(jn,iRS), wmn(jn,iRS), i,lindex(jn,iRS)
+     enddo
+  enddo
+       write(6,*) "iRS = ", iRS, RSectors(iRS) 
+  write(6,*) "all nodes and weights:"
+  do i = 1, sR
+     write(6,*) i, nodesR(i), weightsR(i), weightsDMR(i,i)
+  enddo
+  write(6,*) "do a test integral \int{x^2} from 0 to 10"
+  res1=0d0
+  do i = 1, sR
+     res1 = res1 + weightsR(i)*nodesR(i)**2
+  enddo
+  write(6,*) "... result is:", res1
+  write(6,*) "try it again with matrix math:"
+  write(6,*) dot_product(nodesR,MATMUL(weightsDMR,nodesR))
+
+  fp(:,:,:)=0d0
+
+  do iRS = 1, NRSectors
+     do in = 1, LP
+        do jn = 1, LP
+           call Calcfmnprime(xmn, wmn, iRS, iRS, in, jn, fp(jn,in,iRS), NRSectors, LP)
+           
+!           write(6,*) iRS, in, jn, fp(jn,in,iRS)
+        enddo
+!        fp(:,in,iRS) = fp(:,in,iRS)/sqrt(wmn(in,iRS))
+!        write(6,*)
+     enddo
+     
+     SectorT(:,:,iRS) = matmul(Transpose(fp(:,:,iRS)),matmul(wtemp(:,:,iRS),fp(:,:,iRS)))
+!     call printmatrix(0.5d0*SectorT(:,:,iRS),LP,LP,6)
+!     write(6,*)
+  enddo
   
-!  call testbasis(nodesR, weightsR, sr)
-!  call test1D(TmatR,sr,weightsR,nodesR)
-!  stop
+  ! now construct the basis derivatives (Suno eq. 15)
+!  dXrAll(:,:)=0d0
+  allocate(AllT(LP-1,LP-1,NRSectors,NRSectors))
+  AllT=0d0
+  allocate(TmatTemp(sr-1,sr-1))
+  allocate(TmatR(src,src))
+  TmatR=0d0
+  TmatTemp=0d0
+  do iRS = 1, NRSectors
+     do jRS = 1, NRSectors
+        do im = 1, LP-1
+           do in = 1, LP-1
+              if ( (in.gt.1) .and. (im.gt.1) ) then
+                 AllT(im,in,iRS,jRS) = kdelta(iRS,jRS)*SectorT(im,in,iRS)*(wmn(im,iRS)*wmn(in,iRS))**(-0.5d0)
+              else if( (im.eq.1).and.(in.gt.1) ) then
+                 AllT(im,in,iRS,jRS) = (kdelta(iRS-1,jRS)*SectorT(LP,in,jRS) + kdelta(iRS,jRS)*SectorT(1,in,jRS)) &
+                      *(wmn(in,jRS)*(wmn(LP,iRS-1)+wmn(1,jRS)))**(-0.5d0)
+              else if( (in.eq.1) .and. (im.gt.1) ) then
+                 AllT(im,in,iRS,jRS) = (kdelta(iRS,jRS-1)*SectorT(im,LP,iRS) + kdelta(iRS,jRS)*SectorT(im,1,iRS)) &
+                      *(wmn(im,iRS)*(wmn(LP,jRS-1)+wmn(1,iRS)))**(-0.5d0)
+              else if ( (im.eq.1) .and. (in.eq.1)) then
+                 AllT(im,in,iRS,jRS) = kdelta(iRS,jRS)*SectorT(LP,LP,iRS-1)
+                 AllT(im,in,iRS,jRS) = AllT(im,in,iRS,jRS) + kdelta(iRS-1,jRS)*SectorT(LP,1,iRS-1)
+                 AllT(im,in,iRS,jRS) = AllT(im,in,iRS,jRS) + kdelta(iRS,jRS-1)*SectorT(1,LP,iRS)
+                 AllT(im,in,iRS,jRS) = AllT(im,in,iRS,jRS) + kdelta(iRS,jRS)*SectorT(1,1,iRS)
+                 AllT(im,in,iRS,jRS) = AllT(im,in,iRS,jRS)/sqrt(wmn(LP,iRS-1)+wmn(1,iRS))/sqrt(wmn(LP,jRS-1)+wmn(1,jRS))
+              endif
+              TmatTemp(lindex(im,iRS),lindex(in,jRS)) = AllT(im,in,iRS,jRS)
+              
+           enddo
+        enddo
+     enddo
+  enddo
+  TmatR(:,:) = 0.5d0*TmatTemp(2:sr-1,2:sr-1)
+  deallocate(TmatTemp)
+!!$  allocate(dXr(sR,sRc))
+!!$  dXr(:,:) = 0d0
+!!$  do i = 1, sRc
+!!$     dXr(:,i) = dXrAll(:,i+1)
+!!$  enddo
+!!$  TmatR=0d0
+!!$  TmatR = (0.5d0/mu4)*MATMUL(Transpose(dXr),MATMUL(weightsDMR,dXr))
+
+  !TmatR = (0.5d0/mu4)*MATMUL(dXr,MATMUL(weightsDMR,TRANSPOSE(dXr)))
+  !  write(6,*) "Kinetic energy matrix:"
+  
+  !call printmatrix(TmatR,sRc,sRc,6)
+  
+  !  call testbasis(nodesR, weightsR, sr)
+  call test1D(TmatR,sr,weightsR,nodesR)
+  
+  stop  
   !obtain the adiabatic eigenvalues and eigenvectors 
   LegendreFile='Legendre.dat'
   LegPoints=10
@@ -201,8 +312,8 @@ program SVD
 
   call system_clock(Tend,rate)
   write(6,*) info
-  write(6,*) (Tend-Tstart)/rate
-  write(1,*) '#',(Tend-Tstart)/rate, numstates, Uad(sRc,1,1)
+  write(6,*) dble((Tend-Tstart)/rate)/60.d0, "minuits"
+  write(1,*) '#',dble((Tend-Tstart)/rate)/60.d0, numstates, Uad(sRc,1,1)
 
   do i=1,probsize
      write(1,*) EVals(i)
@@ -276,6 +387,47 @@ subroutine rescale(n,a,b,nodes,weights,xn,wn)
   end do
 end subroutine rescale
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Find the derivative of the DVR basis function using the Eq. (24) in Suno, J. Chem. Phys 134, 064318 (2011)
+! NRS = number of radial sectors
+! ML = number of Lobatto points in each sector
+! returns the derivative of the ith shape function at the jth point
+subroutine Calcfmnprime(x, w, n, np, i, j, res, NRS, ML)
+  implicit none
+  integer n, np, i, j, k,NRS, ML, kd1, kdM
+  double precision res, x(ML,NRS), w(ML,NRS),holder(ML)
+!  call printmatrix(x,ML,NRS,6)
+!  call printmatrix(w,ML,NRS,6)
+  res = 0d0
+
+  if(n.eq.np) then
+     kd1 = 0
+     kdM = 0
+     if (i.eq.ML) kdM = 1
+     if (j.eq.1) kd1 = 1
+     holder = 0d0
+     if(i.eq.j) then
+        res = 0.5d0*(kdM - kd1)/w(i,n) 
+     else
+        do k = 1, ML
+           if((k .ne. i).and.(k.ne.j)) then
+              holder(k)=(x(j,n)-x(k,n))/(x(i,n)-x(k,n))
+           else
+              holder(k)=1d0
+           endif
+        enddo
+        res=PRODUCT(holder)/((x(i,n)-x(j,n)))
+     end if
+  endif
+  
+end subroutine Calcfmnprime
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+integer function kdelta(i,j)
+  integer i, j
+  kdelta=0
+  if(i.eq.j) kdelta=1
+  
+end function kdelta
+
 !find derivitave of the DVR basis at the DVR points
 ! Returns the derivative of the ith basis function at the lth node
 subroutine CalcDX(nodes,i,l,sz,weights,res)
@@ -307,7 +459,7 @@ subroutine makeRSectors(RSectors,NRSectors, Rmin, Rmax)
   double precision RSectors(NRSectors+1), Rmin, Rmax
 
   do iR = 1, NRSectors+1
-     RSectors(iR) = Rmin + (iR-1)/dble(NRSectors) * (Rmax - Rmin)
+     RSectors(iR) = Rmin + (dble(iR-1)/dble(NRSectors)) * (Rmax - Rmin)
   enddo
 end subroutine makeRSectors
 
@@ -380,26 +532,29 @@ end subroutine testbasis
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 subroutine test1D(Tmat,sr,weights,nodes)
   implicit none
-  integer sr, sRc, i, j
+  integer sr, sRc, i, j, numvecs
   double precision Tmat(sr-2,sr-2),V(sr-2,sr-2)
   double precision weights(sr),nodes(sr)
   double precision evals(sr-2), evecs(sr-2,sr-2)
-
+  numvecs=10
   sRc = sr-2
   V=0d0
   do i=1,sRc
-     V(i,i) = 6d0/(2d0*nodes(i+1)**2)
+     !V(i,i) = 6d0/(2d0*nodes(i+1)**2)
+     V(i,i) = 0.5d0*nodes(i+1)**2
   enddo
   call Mydsyev(Tmat+V,sRc,evals,evecs)
-  open(unit = 9, file = "SphericalWell-Evals.dat")
+  open(unit = 9, file = "HO-Evals.dat")
   do i = 1, sRc
      write(9,*) evals(i)
   enddo
   close(9)
-  open(unit=10, file = "SphericalWell-Evecs.dat")
+  open(unit=10, file = "HO-Evecs.dat")
+  write(10,*) nodes(1),(0d0, j=1,numvecs)
   do i = 1, sRc
-     write(10,*) nodes(i+1), (evecs(i,j)/sqrt(weights(i)), j=1,sRc)
+     write(10,*) nodes(i+1), (evecs(i,j)/sqrt(weights(i+1)), j=1,numvecs)
   enddo
+  write(10,*) nodes(sR),(0d0, j=1,numvecs)
   close(10)
 end subroutine test1D
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
--- makefile
+++ makefile
@@ -7,8 +7,8 @@ ARPACK =  -L/usr/local/lib/ -larpack
 INCLUDE =  -I/usr/local/include
 WARN = #-Wall
 
-4BodySVD.x:	4BodySVD.f90 adiabaticSolver.f90 Bsplines.f90 FourBodyPOT.f90 matrix_stuff.o
-	${CMP} ${WARN} ${OPT} ${FREEFORM} matrix_stuff.o 4BodySVD.f90 adiabaticSolver.f90 Bsplines.f90 FourBodyPOT.f90  ${LAPACK} ${ARPACK} ${INCLUDE} 
+4BFEMSVD.x:	4BFEMSVD.f90 adiabaticSolver.f90 Bsplines.f90 FourBodyPOT.f90 matrix_stuff.o
+	${CMP} ${WARN} ${OPT} ${FREEFORM} matrix_stuff.o 4BFEMSVD.f90 adiabaticSolver.f90 Bsplines.f90 FourBodyPOT.f90  ${LAPACK} ${ARPACK} ${INCLUDE} 
 
 matrix_stuff.o: matrix_stuff.f
 	${CMP} ${WARN} ${F132FORM} ${OPTFLAG} -c matrix_stuff.f
